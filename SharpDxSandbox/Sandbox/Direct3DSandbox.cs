09eef714e1c2559dd97d47ef4aa3ae1c48fb26dd
Viacheslav Nedavnii <vny@tobii.com>
2023-05-02 10:43:20 +0000

    DirectX Draw octahedron as triangle list to show problem with duplicated points which will be addressed with DrawIndexed

--- SharpDxSandbox/Sandbox/Direct3DSandbox.cs
+++ SharpDxSandbox/Sandbox/Direct3DSandbox.cs
@@ -12,7 +12,7 @@ using Device = SharpDX.Direct3D11.Device;
 
 namespace SharpDxSandbox.Sandbox;
 
-public class Direct3DSandbox
+public static class Direct3DSandbox
 {
     public static async Task StartTriangle()
     {
@@ -46,15 +46,21 @@ public class Direct3DSandbox
                     {
                         ColoredVertex[] vertices =
                         {
+                            new(new(0f, 0.9f, 0f), Color.Blue),
+                            new(new(-0.5f, -0.3f, 0f), Color.Red),
+                            new(new(-0.3f, 0.7f, 0f), Color.Green),
 
-                            new(new(0f, 0f, 0f), Color.Red),
-                            new(new(-1f, -1f, 0f), Color.Green),
-                            new(new(-1f, 1.1f, 0f), Color.Blue),
+                            new(new(0f, 0.9f, 0f), Color.Red),
+                            new(new(0.3f, 0.7f, 0f), Color.Green),
+                            new(new(0.5f, -0.3f, 0f), Color.Blue),
                             
-                            new(new(1f, -1f, 0f), Color.Red),
-                            new(new(0f, 0f, 0f), new Color(1.0f, 1.0f, 0.0f)),
-                            new(new(1f, 1f, 0f), new (1.0f, 0.0f, 1.0f))
+                            new(new(0f, 0.9f, 0f), Color.Blue),
+                            new(new(0.5f, -0.3f, 0f), Color.Red),
+                            new(new(-0.5f, -0.3f, 0f), Color.Green),
                             
+                            new(new(0.5f, -0.3f, 0f), Color.Red),
+                            new(new(0f, -0.7f, 0f), Color.Green),
+                            new(new(-0.5f, -0.3f, 0f), Color.Blue),
                         };
 
                         using var vertexDataStream = DataStream.Create(vertices, true, false);
@@ -102,7 +108,8 @@ public class Direct3DSandbox
                         while (!cancellation.IsCancellationRequested)
                         {
                             device.ImmediateContext.OutputMerger.SetRenderTargets(renderTargetView);
-                            device.ImmediateContext.ClearRenderTargetView(renderTargetView, new RawColor4(0.5f, (float)Math.Sin(colorShift), 0, 1f));
+                            var color = new RawColor4(0.5f, (float)Math.Sin(colorShift), 0, 1f);
+                            device.ImmediateContext.ClearRenderTargetView(renderTargetView, new RawColor4(1f, 1f, 1f, 1f));
                             device.ImmediateContext.Draw(vertices.Length, 0);
 
                             var presentResult = swapChain.Present(1, PresentFlags.None);
@@ -114,7 +121,7 @@ public class Direct3DSandbox
                             colorShift += 0.03f;
                         }
                     }
-                    catch (SEHException e)
+                    catch (SEHException)
                     {
                         logger.FlushMessages();
                     }
@@ -125,7 +132,7 @@ public class Direct3DSandbox
 
     [StructLayout(LayoutKind.Sequential)]
     private record struct ColoredVertex(Vertex Vertex, Color Color);
-   
+
     [StructLayout(LayoutKind.Sequential)]
     private readonly record struct Vertex(float X, float Y, float Z);
 
@@ -133,7 +140,9 @@ public class Direct3DSandbox
     private readonly record struct Color(float R, float G, float B)
     {
         public static Color Red => new(1f, 0f, 0f);
+
         public static Color Green => new(0f, 1f, 0f);
+
         public static Color Blue => new(0f, 0f, 1f);
     }
 }
